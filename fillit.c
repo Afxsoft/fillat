115d8a6344c63c6057d6a89b1c677cb73ed14908
Nicolas LAGACHE <nlagache@e1r4p6.42.fr>
2015-12-09 14:15:59 +0100

    check form

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: aouloube <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 13:54:00 by aouloube          #+#    #+#             */
-/*   Updated: 2015/12/08 17:27:43 by aouloube         ###   ########.fr       */
+/*   Updated: 2015/12/09 12:41:02 by nlagache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,7 @@ void	ft_readfile(char *file, int fd)
 	i = 0;
 	while (read(fd, &tmp, 1))
 	{
-		/*if (i == 20)
-		{
-			y++;
-			i = 0;
-		}
-		else
-		{
-			if (tmp != '.' && tmp != '#' && tmp != '\n')
-				ft_error("The character used is not allowed");
-			buff[y][i] = tmp;
-			printf("%c", tmp);
-		}*/
-		
-			file[i] = tmp;
+		file[i] = tmp;
 		i++;
 	}
 	file[i] = '\0';
@@ -62,20 +49,27 @@ void	ft_tab2m(char *file, char buff[26][20])
 	size = ft_strlen(file);
 	while (size > 0)
 	{
-		if (y == 0)
 		if (file[i] != '.' && file[i] != '#' && file[i] != '\n')
 			ft_error("The character used is not allowed");
 		buff[y][v] = file[i];
+		if (size == 1 && file[i] == '\n')
+			ft_error("There is a \n at the last element");
+		else if (size == 1 && v != 18)
+			ft_error("capout");
 		if (file[i] == '\n' && file [i - 1] == '\n')
 		{
 			y++;
 			v = 0;
-		}else
+		}
+		else
+		{
+			if (v > 20)
+				ft_error("One or more tetraminos char is != 20");
 			v++;
+		}
 		i++;
 		size--;
 	}
-	printf("%s", buff[0]);
 }
 
 void	ft_tchecker(char b[26][20])
@@ -120,6 +114,8 @@ void	ft_diesechecker(char b[26][20])
 	while (b[y][i])
 	{
 		count = (b[y][i] == '#') ? count + 1 : count;
+		if (b[y][i] == '#')
+			b[y][i] = y + 'A';		
 		y = (i == 20) ? y + 1 : y;
 		if (i == 19)
 			if (count != 4)
@@ -143,36 +139,32 @@ void	ft_charchecker(char b[26][20])
 	while (b[y][i])
 	{
 		count++;
-		y = (i == 21) ? y + 1 : y;
+		y = (i == 20) ? y + 1 : y;
 		if (b[y][i] == '\n')
 		{
 			if (count != 5 && count != 1)
 				ft_error("Number of Tretriminos is incorrect");
 			count = 0;
 		}
-		i = (i == 21) ? 0 : i;
+		i = (i == 20) ? 0 : i;
 		i++;
 	}
 }
 
 void	ft_ttry(char buff[26][20])
 {
-//	char	tab[4][5];
 	int		i;
 	int		y;
 
 	i = 0;
 	y = 0;
-	
-	//printf("%s", buff[1]);
 	while (buff[y][i])
 	{
-		if (i == 21 )
+		if (i == 21)
 		{
 			i = 0;
 			y++;
-			}
-		
+		}
 		i++;
 	}
 }
@@ -186,12 +178,11 @@ int		main(int argc, char **argv)
 	if (argc == 2)
 	{
 		ft_readfile(file, open(argv[1], O_RDONLY));
-		//printf("%s", file);
 		ft_tab2m(file, buff);
-		printf("%s", buff[0]);
-		//ft_tchecker(buff);
-		//ft_diesechecker(buff);
-		//ft_charchecker(buff);
+		ft_tchecker(buff);
+		ft_diesechecker(buff);
+		ft_putstr(buff[0]);
+		ft_charchecker(buff);
 		//ft_ttry(buff);
 	}
 	return (0);
